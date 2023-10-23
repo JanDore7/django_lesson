7c5af29853d8830f97e9ee54594630907e49c877
JanDore7 <jan_dore2030@proton.me>
2023-10-23 23:24:23 +0300

    404 Страница не найдена

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 
@@ -11,8 +11,14 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
+    if request.GET :
+        print(request.GET)
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
 
 
 def archive(request, year):
     return HttpResponse(f'<h1>Архив по годам</h1><p>год: {year}</p>')
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
