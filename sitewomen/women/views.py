5033559a4774f664859f1380307e647b36650fc4
JanDore7 <jan_dore2030@proton.me>
2023-10-24 00:52:38 +0300

    raise Http404()

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 
@@ -17,6 +17,8 @@ def categories_by_slug(request, cat_slug):
 
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
     return HttpResponse(f'<h1>Архив по годам</h1><p>год: {year}</p>')
 
 
