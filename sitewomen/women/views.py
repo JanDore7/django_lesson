7bf05a18ee63972d596221b3f43b4d3022bb8f8a
JanDore7 <jan_dore2030@proton.me>
2023-10-25 04:42:38 +0300

    render_to_string и каталог для шаблонов

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 
 def index(request):
-    return HttpResponse('Страница приложения women')
+    t = render_to_string('women/index.html')
+    return HttpResponse(t)
 
 
 def categories(request, cat_id):
@@ -13,7 +14,7 @@ def categories(request, cat_id):
 
 
 def categories_by_slug(request, cat_slug):
-    if request.GET :
+    if request.GET:
         print(request.GET)
     return HttpResponse(f'<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>')
 
